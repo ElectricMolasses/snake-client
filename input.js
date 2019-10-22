d58deacd54ced824a6f8b40e876fed396f46d292
Robert Beckett <robert.beckett@onion.red>
2019-10-22 02:16:35 +0000

    Passed connection object across play and to input, and then had input write movement calls to the server based on user input.

--- input.js
+++ input.js
@@ -1,17 +1,29 @@
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (key) => {
-    handleUserInput(key);
+    handleUserInput(key, conn);
   });
   return stdin;
 };
 
-const handleUserInput = function(input) {
+const handleUserInput = function(input, conn) {
   if (input === '\u0003') {
     process.exit();
+  }
+  if (input === 'w') {
+    conn.write('Move: up');
+  }
+  if (input === 's') {
+    conn.write('Move: down');
+  }
+  if (input === 'a') {
+    conn.write('Move: left');
+  }
+  if (input === 'd') {
+    conn.write('Move: right');
   }
 };
 

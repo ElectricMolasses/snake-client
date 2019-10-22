760ba612a7c9ea02b8a415e485957017f44856b4
Robert Beckett <robert.beckett@onion.red>
2019-10-22 02:25:33 +0000

    Created movement constants and exported.

--- constants.js
+++ constants.js
@@ -1,7 +1,25 @@
 const IP = 'localhost';
 const PORT = 50541;
 
+const MOVE_UP_KEY = 'w';
+const MOVE_DOWN_KEY = 's';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_RIGHT_KEY = 'd';
+
+const MOVE_UP_COMMAND = 'Move: up';
+const MOVE_DOWN_COMMAND = 'Move: down';
+const MOVE_LEFT_COMMAND = 'Move: left';
+const MOVE_RIGHT_COMMAND = 'Move: right';
+
 module.exports = {
   IP,
-  PORT
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_UP_COMMAND,
+  MOVE_DOWN_COMMAND,
+  MOVE_RIGHT_COMMAND,
+  MOVE_LEFT_COMMAND,
 };

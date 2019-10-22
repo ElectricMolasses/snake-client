b0f07285ed025629e5259cdfad0e7faf647ca6fa
Robert Beckett <robert.beckett@onion.red>
2019-10-22 01:57:19 +0000

    Created stdin within function, attached escape shortcut Ctrl+C to exit program.

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const { connect } = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+
+const handleUserInput = function() {
+  
+};
+
 console.log('Connecting ...');
 connect();
+setupInput();

ec202a5641b25e3934db3b1eb89ec8b2a13c7b42
Robert Beckett <robert.beckett@onion.red>
2019-10-22 01:48:31 +0000

    Added move commands as comment.

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Connection established.');
     conn.write('Name: ROK');
+    
   });
 
   conn.on('data', (data) => {
@@ -23,3 +24,8 @@ const connect = function() {
 module.exports = {
   connect
 };
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
